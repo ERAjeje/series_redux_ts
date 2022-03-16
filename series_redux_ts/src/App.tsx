6ac57cc06a840bfa55d2121a5a2980c2f86a5186
Edson Ajeje <edson.ajeje@terra.com.br>
2022-03-16 18:33:57 -0300

    preparacao do App para os testes do redux

--- series_redux_ts/src/App.tsx
+++ series_redux_ts/src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import Card from './app/components/Card';
+import List from './app/components/List';
+import Pill from './app/components/Pill';
+import Serie from './app/components/Serie';
+import { addCategory, Category } from './app/features/category/categorySlice';
+import { addSerie, Serie as SerieType } from './app/features/serie/serieSlice';
+import { RootState } from './app/store';
 
 function App() {
+  const series = useSelector((state: RootState) => state.series.series);
+  const categories = useSelector((state: RootState) => state.categories.categories)
+  const dispatch = useDispatch();
+  const handleAdd = () => dispatch(addCategory({
+    data: {
+      title: 'Terror',
+      active: false
+    }
+  }))
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(categories)}
+      <List>
+        {
+          series.map((item: SerieType) => <Card><Serie title={item.title} categories={item.categories} watched={item.whatched} /></Card>)
+        }
+      </List>
+      <button onClick={handleAdd}>Adicionar</button>
     </div>
   );
 }
