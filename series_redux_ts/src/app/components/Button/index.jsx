900e26b321e09a6538ffdfee0be1dfc6b8c96dea
Edson Ajeje <edson.ajeje@terra.com.br>
2022-03-17 19:11:35 -0300

    criacao de componentes Button e Input

---
+++ series_redux_ts/src/app/components/Button/index.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+    color: white;
+    background-color: darkorange;
+    border: 1px solid darkorange;
+    border-radius: 55px;
+    padding: 10px 30px;
+    margin-top: 10px;
+    font-size: 1rem;
+    font-weight: lighter;
+    cursor: pointer;
+`;
+
+export default Button;
---
+++ series_redux_ts/src/app/components/Input/index.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+    height: 40px;
+    width: 100%;
+    border: none;
+    border-bottom: 1px solid purple;
+    margin-bottom:  20px;
+    margin-right: 10px;
+    margin-left: 10px;
+    font-size: 1rem;
+    padding: 10px 25px;
+`;
+
+export default Input;
