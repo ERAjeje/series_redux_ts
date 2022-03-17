a2f78726a85b69fbbecc982c0f970bfe04dcc619
Edson Ajeje <edson.ajeje@terra.com.br>
2022-03-17 19:14:35 -0300

    melhoria de css

--- series_redux_ts/src/app/components/Card/index.jsx
+++ series_redux_ts/src/app/components/Card/index.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Card = styled.div`
     width: 150px;
-    height: 150px;
+    height: 190px;
     box-shadow: 1px 1px 6px  black;
     border-radius: 10px;
     margin: 10px;
