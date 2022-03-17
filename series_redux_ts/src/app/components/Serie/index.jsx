c4e183b596b5f15bfb9b04cda9ff106a450bc013
Edson Ajeje <edson.ajeje@terra.com.br>
2022-03-17 19:12:45 -0300

    refatoracao do componenete Serie com redux

--- series_redux_ts/src/app/components/Serie/index.jsx
+++ series_redux_ts/src/app/components/Serie/index.jsx
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
 import eye from '../../assets/eye-solid.svg';
 import eyeSleep from '../../assets/eye-slash-solid.svg'
+import { useDispatch, useSelector } from 'react-redux';
+import { changeSerie } from '../../features/serie/serieSlice';
 
 const Image = styled.img`
     width: 35px;
     margin: 6px;
+    cursor: pointer;
+`;
 
+const Category = styled.span`
+    margin: 5px;
+    font-size: 15px;
 `;
 
 const Body = styled.div`
@@ -13,13 +20,30 @@ const Body = styled.div`
     height: 100%;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
+`;
+
+const Container = styled.div`
+    display: flex;
+    width: 150px;
+    margin: 5px;
+    flex-wrap: wrap;
+    justify-content: inherit;
+    align-content: inherit;
 `;
 
 export default ({ title, categories, watched }) => {
+    const serie = useSelector(state => state.series.series);
+    const dispatch = useDispatch();
+    const handleClick = () => dispatch(changeSerie({ title, value: !watched }))
+
     return <Body>
-        <div><div>{title}</div> {watched ? <Image src={eye} /> : <Image src={eyeSleep} />}</div>
-        {
-            categories.map(item => <span>{item}</span>)
-        }
+        <Container>{title}</Container>
+        <Container>{watched ? <Image onClick={handleClick} src={eye} /> : <Image onClick={handleClick} src={eyeSleep} />}</Container>
+        <Container>
+            {
+                categories.map(item => <Category>{item}</Category>)
+            }
+        </Container>
     </Body>
 }
