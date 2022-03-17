a6522fa028f1e55ec095682e7f4d3192a7b85b22
Edson Ajeje <edson.ajeje@terra.com.br>
2022-03-17 19:12:59 -0300

    melhoria de css

--- series_redux_ts/src/app/components/List/index.jsx
+++ series_redux_ts/src/app/components/List/index.jsx
@@ -2,12 +2,29 @@ import styled from 'styled-components';
 
 const List = styled.div`
     display: flex;
-    width: 100%;
+    width: 85%;
     flex-wrap: wrap;
     flex-direction: ${props => props.direction === 'column' ? 'column' : 'row'};
-    height: ${props => props.height ?? '255px'};
+    height: ${props => props.height ?? '90vh'};
     overflow-y: scroll;
-    padding: 10px;
+    justify-content: ${props => props.justify !== undefined ? props.justify : 'center'};
+    align-content: ${props => props.align !== undefined ? props.align : 'center'};
+    padding: 10px 45px;
+    margin: 0 10%;
+
+    ::-webkit-scrollbar {
+        width: 1px;               /* width of the entire scrollbar */
+    }
+
+    ::-webkit-scrollbar-track {
+        background: white;        /* color of the tracking area */
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: blue;    /* color of the scroll thumb */
+        border-radius: 40px;       /* roundness of the scroll thumb */
+        border: 1px solid purple;  /* creates padding around scroll thumb */
+    }
 `;
 
 export default List;
